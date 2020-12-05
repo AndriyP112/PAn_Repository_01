0dc3ee486f28d1d052701a3121a90f4d305570a5
AndriyP112 <andriyp112@gmail.com>
2020-12-05 14:06:22 +0200

    PAn_01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("commit4");
-        System.out.println("commit4");
-        System.out.println("commit4");
-        System.out.println("commit5");
-        System.out.println("commit5");
-        System.out.println("commit5");
+        System.out.println("Test");
+
 
     }
 }
