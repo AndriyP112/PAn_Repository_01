b667004d22cd80fdeab64d9f32bf9650f6a86039
AndriyP112 <andriyp112@gmail.com>
2020-12-05 13:03:59 +0200

    commit4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("commit4");
+        System.out.println("commit4");
+        System.out.println("commit4");
 
     }
 }
